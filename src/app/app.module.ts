a2756fce6656f7360061074b4f4925ed7125dcdd
AndreasMuellerAtStuttart <a.mueller3@gmx.net>
2019-06-24 15:28:57 +0200

    re-factored font to make it more readable, some layout corrections and addition of Privacy page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { PoetryComponent } from './poetry/poetry.component';
 import { MusicComponent } from './music/music.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { DataCollectionAndPrivacyComponent } from './data-collection-and-privacy/data-collection-and-privacy.component';
 
 const appRoutes: Routes = [
   {path: "", component: HomeComponent},
@@ -22,7 +23,8 @@ const appRoutes: Routes = [
   {path: "poetry", component: PoetryComponent},
   {path: "music", component: MusicComponent},
   {path: "contact", component: ContactComponent},
-  {path: "about", component: AboutComponent}
+  {path: "about", component: AboutComponent},
+  {path: "data-collection-and-privacy", component: DataCollectionAndPrivacyComponent}
 ];
 
 @NgModule({
@@ -35,7 +37,8 @@ const appRoutes: Routes = [
     PoetryComponent,
     MusicComponent,
     ContactComponent,
-    AboutComponent
+    AboutComponent,
+    DataCollectionAndPrivacyComponent
   ],
   imports: [
     BrowserModule,
